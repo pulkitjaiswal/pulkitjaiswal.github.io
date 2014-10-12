7f02de805ec5b04398e6418cb4081a58d2a9bc8c
Pulkit Jaiswal <pulkitj@icloud.com>
2014-10-13 02:27:04 +0800

    stuff

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/writings/"
+http_images_path = "/writings/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/writings/fonts"
+css_dir = "public/writings/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
